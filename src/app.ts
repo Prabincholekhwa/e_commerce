025697d313914367a0a21b3adb54f023fb7d6df4
PrabinCholekhwa <prabincholex@gmail.com>
2024-11-22 23:36:10 +0545

    set up database instance connection

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express, { Express } from 'express';
 import buildExpressServer from './lib/express';
+import { database } from './database/config/connection';
 
 async function main() {
   try {
     const app: Express = express();
     buildExpressServer(app);
+    await database.connection();
   } catch (error: any) {
     console.log('err', error);
   }
